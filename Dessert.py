cabbfde12b467d5c41fe47eaccf98a0e08f5a4b1
IkennaCodes <ikeandadanjoku@gmail.com>
2025-02-02 13:22:55 +0000

    Update Dessert.py

--- Dessert.py
+++ Dessert.py
@@ -8,7 +8,7 @@ t.goto(-300,-100)
 t.pendown()
 
 #sand box
-t.color("#EEDC9A")
+t.color("#C2B280","#EEDC9A")
 t.begin_fill()
 for x in range (2):
     t.forward(600)
@@ -18,7 +18,7 @@ for x in range (2):
 t.end_fill()
 
 #sand particles
-for x in range (60):
+for x in range (30):
     t.penup()
     x = random.randint(-300,300)
     y = random.randint(-300,-100)
@@ -27,4 +27,33 @@ for x in range (60):
     t.color("#C2B280")
     t.dot()
 
+
+#triangle
+t.penup()
+t.goto(-200,-100)
+t.pendown()
+t.color("#C2B280","#EEDC9A")
+t.begin_fill()
+for x in range (3):
+    t.forward(200)
+    t.left(120)
+t.end_fill()
+
+#triangle lines
+
+t.penup()
+t.goto(-177,-60)
+t.pendown()
+t.forward(155)
+
+t.penup()
+t.goto(-153,-20)
+t.pendown()
+t.forward(107)
+
+t.penup()
+t.goto(-130,20)
+t.pendown()
+t.forward(62)
 t.mainloop()
+
